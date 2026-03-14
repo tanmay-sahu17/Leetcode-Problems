65e576edf67138a0cf31425e5c2d736ac961e4e5
TANMAY SAHU <tanmay.sahu2023@ssipmt.com>
2026-03-14 21:25:29 +0530

    Time: 0 ms (100%), Space: 9 MB (64.49%) - LeetHub

---
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string ans = "";
+    int count = 0;
+
+    void solve(string curr, int n, int k){
+        
+        if(curr.size() == n){
+            count++;
+            if(count == k){
+                ans = curr;
+            }
+            return;
+        }
+
+        for(char ch : {'a','b','c'}){
+            
+            if(curr.empty() || curr.back() != ch){
+                solve(curr + ch, n, k);
+            }
+            
+            if(ans != "") return;
+        }
+    }
+
+    string getHappyString(int n, int k) {
+        
+        solve("", n, k);
+        return ans;
+    }
+};
