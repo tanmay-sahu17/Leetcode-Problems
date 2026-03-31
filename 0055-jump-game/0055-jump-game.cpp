379aa812e66fd2ecabcc66bec34e5224da1ade2a
TANMAY SAHU <tanmay.sahu2023@ssipmt.com>
2026-03-31 09:37:11 +0530

    Time: 4 ms (21.2%), Space: 52.3 MB (81.99%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxjump=0;
+        for(int i=0;i<nums.size();i++){
+              if(i>maxjump)return false;
+            if(nums[i]+i>maxjump){
+                maxjump=nums[i]+i;
+            }
+          
+        }
+        return true;
+        
+    }
+};
