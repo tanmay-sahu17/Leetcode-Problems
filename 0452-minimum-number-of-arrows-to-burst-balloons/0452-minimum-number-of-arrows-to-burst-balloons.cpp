a2a976a97bfefb378f5b9f665ebc621fa1ae4b8f
TANMAY SAHU <tanmay.sahu2023@ssipmt.com>
2026-03-31 15:34:20 +0530

    Time: 51 ms (42.26%), Space: 93.8 MB (74.07%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        sort(points.begin(),points.end(),[](vector<int>&a,vector<int>&b){
+            return a[1]<b[1];
+        });
+        int arrows=1;
+        int point=points[0][1];
+
+        for(int i=1;i<points.size();i++){
+            if(points[i][0]>point){
+                arrows++;
+                point=points[i][1];
+            }
+        }
+        return arrows;
+    }
+};
