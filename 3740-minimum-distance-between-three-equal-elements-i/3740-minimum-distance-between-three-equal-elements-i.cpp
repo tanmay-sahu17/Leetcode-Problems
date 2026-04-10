6450b70a58d759fe2b75ab2857373a5522960547
TANMAY SAHU <tanmay.sahu2023@ssipmt.com>
2026-04-10 22:33:52 +0530

    Time: 4 ms (52.52%), Space: 29.7 MB (40.76%) - LeetHub

---
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums) {
+        unordered_map<int, vector<int>> mp;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            mp[nums[i]].push_back(i);
+        }
+
+        int ans = INT_MAX;
+
+        for(auto &it : mp) {
+            vector<int>& v = it.second;
+            
+            if(v.size() >= 3) {
+                for(int i = 0; i + 2 < v.size(); i++) {
+                    ans = min(ans, 2 * (v[i + 2] - v[i]));
+                }
+            }
+        }
+
+        return ans == INT_MAX ? -1 : ans;
+    }
+};
