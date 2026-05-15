bec76e8b72e008bc27a35dfaad79fe3538d0935c
TANMAY SAHU <tanmay.sahu2023@ssipmt.com>
2026-05-15 23:44:09 +0530

    Time: 0 ms (100%), Space: 27.8 MB (5.93%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
     bool isGood(vector<int>& nums) {
-        int mx = *max_element(nums.begin(), nums.end());
-
-        // size must be mx + 1
-        if(nums.size() != mx + 1)
-            return false;
-
-        vector<int> freq(mx + 1, 0);
-
-        for(int x : nums) {
-            // invalid number
-            if(x < 1 || x > mx)
-                return false;
-            freq[x]++;
+        int n=nums.size();
+        map<int,int>mp;
+        for(auto i:nums){
+            mp[i]++;
         }
-
-        // 1 to mx-1 should appear once
-        for(int i = 1; i < mx; i++) {
-            if(freq[i] != 1)
-                return false;
-        }
-        // mx should appear twice
-        return freq[mx] == 2;
+        if(mp[n-1]!=2)return false;
+      for(int i=1;i<n-1;i++){
+        if(mp[i]!=1)return false;
+      }
+      return true;
+        
     }
 };
