6de7269966fecc31825478674e4f2841a0c00432
TANMAY SAHU <tanmay.sahu2023@ssipmt.com>
2026-06-15 22:39:34 +0530

    Time: 1664 ms (8.98%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -16,7 +16,7 @@ UNION ALL
     JOIN MovieRating mr
         ON m.movie_id = mr.movie_id
     WHERE DATE_FORMAT(mr.created_at, '%Y-%m') = '2020-02'
-    GROUP BY m.movie_id, m.title
+    GROUP BY m.title
     ORDER BY AVG(mr.rating) DESC, m.title ASC
     LIMIT 1
 );
