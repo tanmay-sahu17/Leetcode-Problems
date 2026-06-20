4bd94ab351fef0a2e62dbbeb62d4d8eb0801a41b
TANMAY SAHU <tanmay.sahu2023@ssipmt.com>
2026-06-20 13:32:26 +0200

    Time: 22 ms (88.81%), Space: 16.5 MB (65.28%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,29 +1,47 @@
 class Solution {
 public:
-    void dfs(int i,int j,vector<vector<char>>& grid, vector<vector<bool>>&vis,int m,int n){
-        if(i<0||j<0||i>=m||j>=n||vis[i][j]==true||grid[i][j] == '0')return;
+    void dfs(int i, int j,
+             vector<vector<char>>& grid,
+             vector<vector<bool>>& visited) {
 
-        vis[i][j]=true;
+        int m = grid.size();
+        int n = grid[0].size();
 
-        dfs(i-1,j,grid,vis,m,n);
-        dfs(i,j+1,grid,vis,m,n);
-        dfs(i+1,j,grid,vis,m,n);
-        dfs(i,j-1,grid,vis,m,n);
+        if (i < 0 || j < 0 || i >= m || j >= n ||
+            grid[i][j] == '0' || visited[i][j])
+            return;
+
+        visited[i][j] = true;
+
+        dfs(i + 1, j, grid, visited);
+        dfs(i - 1, j, grid, visited);
+        dfs(i, j + 1, grid, visited);
+        dfs(i, j - 1, grid, visited);
     }
+
     int numIslands(vector<vector<char>>& grid) {
-        int island=0;
-        int m=grid.size();
-        int n=grid[0].size();
-       vector<vector<bool>> vis(m, vector<bool>(n, false));
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-               if(grid[i][j]=='1'&&!vis[i][j]){
-                dfs(i,j,grid,vis,m,n);
-                island++;
-               }
+        int m = grid.size();
+        int n = grid[0].size();
+
+        int island = 0;
+
+        vector<vector<bool>> visited(
+            m,
+            vector<bool>(n, false)
+        );
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (grid[i][j] == '1' &&
+                    visited[i][j] == false) {
+
+                    dfs(i, j, grid, visited);
+                    island++;
+                }
             }
         }
+
         return island;
-        
     }
 };
