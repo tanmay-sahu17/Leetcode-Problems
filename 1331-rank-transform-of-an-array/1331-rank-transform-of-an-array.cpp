1bc4e165512bc992cd805bb95dc83d79633b6353
TANMAY SAHU <tanmay.sahu2023@ssipmt.com>
2026-07-13 22:18:30 +0530

    Time: 81 ms (10.33%), Space: 44.4 MB (43.94%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+
+        vector<int> temp=arr;
+        map<int,int> mp;
+
+        sort(temp.begin(),temp.end());
+
+        int rank=1;
+
+        for(int i=0;i<temp.size();i++){
+            if(mp.find(temp[i])==mp.end()){
+                mp[temp[i]]=rank++;
+            }
+        }
+
+        for(int i=0;i<arr.size();i++){
+            arr[i]=mp[arr[i]];
+        }
+
+        return arr;
+    }
+};
