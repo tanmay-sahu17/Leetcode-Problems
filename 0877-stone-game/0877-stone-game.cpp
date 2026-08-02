1a123de12785db60c849863b5d10aa84994c778a
TANMAY SAHU <tanmay.sahu2023@ssipmt.com>
2026-08-02 22:06:35 +0530

    Time: 18 ms (15.41%), Space: 19.5 MB (27.79%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
+
+    int solve(vector<int>& piles, int i, int j)
+    {
+        if(i > j) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int takei = piles[i] + min(solve(piles, i + 2, j), solve(piles, i + 1, j - 1));
+        int takej = piles[j] + min(solve(piles, i, j - 2),solve(piles, i + 1, j - 1));
+
+        return dp[i][j] = max(takei, takej);
+    }
+
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n = piles.size();
+        dp.assign(n, vector<int>(n, -1));
+        int sum = accumulate(begin(piles), end(piles), 0);
+        int alice = solve(piles, 0, n - 1);
+        return alice > sum / 2;
     }
 };
