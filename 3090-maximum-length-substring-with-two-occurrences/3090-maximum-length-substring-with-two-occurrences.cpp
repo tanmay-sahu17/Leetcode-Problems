174a66f2519a9c28957ce28a4c62303640618e27
TANMAY SAHU <tanmay.sahu2023@ssipmt.com>
2026-08-14 14:50:30 +0530

    Time: 4 ms (28.78%), Space: 9.6 MB (44.69%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
@@ -6,14 +6,15 @@ public:
          int n=s.size();
          int maxlen=INT_MIN;
          map<int,int>mp;
-
          for(int r=0;r<n;r++){
             mp[s[r]]++;
             while(mp[s[r]]>2){
                 mp[s[l]]--;
                 l++;
             }
-            maxlen=max(r-l+1,maxlen);
+            if(r-l+1>maxlen){
+                maxlen=r-l+1;
+            }
          }
         return maxlen;
     }
