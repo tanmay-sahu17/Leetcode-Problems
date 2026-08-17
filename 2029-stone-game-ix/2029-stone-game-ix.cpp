76a723beb80b244f15e5a1cfc7bc530987527823
TANMAY SAHU <tanmay.sahu2023@ssipmt.com>
2026-08-17 16:05:06 +0530

    Time: 3 ms (76.02%), Space: 131.2 MB (69.9%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt[3] = {};
+
+        for (int x : stones)
+            cnt[x % 3]++;
+
+        if (cnt[0] % 2 == 0)
+            return cnt[1] && cnt[2];
+
+        return abs(cnt[1] - cnt[2]) > 2;
+    }
+};
