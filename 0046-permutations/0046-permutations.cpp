6d4042ed2601418e69036aff9930fa3b79785020
TANMAY SAHU <tanmay.sahu2023@ssipmt.com>
2026-08-31 13:03:07 +0530

    Time: 1 ms (41.95%), Space: 10.4 MB (88.94%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
      
-      vector<vector<int>>ans;
-      void solve(vector<int>&nums,int index){
+      
+   void solve(vector<int>&nums,int index,vector<vector<int>>&ans){
         if(index==nums.size()){
             ans.push_back(nums);
         }
 
         for(int i=index;i<nums.size();i++){
             swap(nums[index],nums[i]);
-            solve(nums,index+1);
+            solve(nums,index+1,ans);
             swap(nums[index],nums[i]);
         }
+      
       }
 
     vector<vector<int>> permute(vector<int>& nums) {
-         solve(nums,0);
-        return ans;
+        vector<vector<int>>ans;
+          solve(nums,0,ans);
+          return ans;
+       
     }
 };
