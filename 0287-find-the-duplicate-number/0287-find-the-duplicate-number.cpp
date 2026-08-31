ec5781718a3fdf418e86993d0f40dbf7f96c6821
TANMAY SAHU <tanmay.sahu2023@ssipmt.com>
2026-08-31 15:28:12 +0530

    Time: 232 ms (5.17%), Space: 107.3 MB (6.44%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        map<int,int>mp;
+
+        for(auto i:nums){
+            mp[i]++;
+        }
+
+        for(auto i:mp){
+            if(i.second>=2){
+                return i.first;
+            }
+        }
+        return -1;
+    }
+};
