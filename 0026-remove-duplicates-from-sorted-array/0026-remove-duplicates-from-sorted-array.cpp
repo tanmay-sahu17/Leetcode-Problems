8a3159e750f382f4423e9b71feaa50b58e337487
TANMAY SAHU <tanmay.sahu2023@ssipmt.com>
2026-09-01 13:46:59 +0530

    Time: 0 ms (100%), Space: 22.7 MB (52.91%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -5,7 +5,7 @@ public:
         int n = nums.size();
         if(n == 0)
             return 0;
-        int j = 1;
+        int j =1;
 
         for(int i = 1; i < n; i++) {
             if(nums[i]!=nums[j-1]) {
