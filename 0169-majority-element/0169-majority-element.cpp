f5491ddf56bab267f740c69f86f0ac2c8e07a319
TANMAY SAHU <tanmay.sahu2023@ssipmt.com>
2026-09-02 10:44:00 +0530

    Time: 0 ms (100%), Space: 28.2 MB (75.79%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate=nums[0];
+        int count=1;
+        
+        int n=nums.size();
+        for(int i=1;i<n;i++){
+           if(nums[i]==candidate){
+            count++;
+           }
+           else{
+            count--;
+           }
+
+           if(count==0){
+            candidate=nums[i];
+            count=1;
+           }
+        }
+        return candidate;
+    }
+};
