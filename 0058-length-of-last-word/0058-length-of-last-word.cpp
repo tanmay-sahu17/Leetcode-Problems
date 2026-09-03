c302788a7fe948a08e2d16e465576e1172981c4f
TANMAY SAHU <tanmay.sahu2023@ssipmt.com>
2026-09-03 11:23:50 +0530

    Time: 0 ms (100%), Space: 9.1 MB (8.65%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        
         while(s.back()==' '){
             s.pop_back();
         }
-        int cnt=0;
+        int len=0;
         while(!s.empty()&&s.back()!=' '){
-            cnt++;
-            s.pop_back();
+             len++;
+             s.pop_back();
         }
-        return cnt;
+        return len;
     }
 };
