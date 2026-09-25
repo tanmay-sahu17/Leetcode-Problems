057f016d13f753c671265eedc7318ab680d9c170
TANMAY SAHU <tanmay.sahu2023@ssipmt.com>
2026-09-25 09:13:15 +0530

    Time: 0 ms (100%), Space: 22.1 MB (61.41%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==NULL){
+            return 0;
+        }
+        if(!root->left&&!root->right){
+            return 1;
+        }
+        int left=maxDepth(root->left);
+        int right=maxDepth(root->right);
+         
+         int maxdepth=1+max(left,right);
+
+         return maxdepth;
+    }
+};
