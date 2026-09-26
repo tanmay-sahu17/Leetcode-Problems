4c1702b2f0e739dd8cc8a9368fb55c65fb72a2b0
TANMAY SAHU <tanmay.sahu2023@ssipmt.com>
2026-09-26 11:24:15 +0530

    Time: 0 ms (100%), Space: 31.4 MB (39.98%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    int leftHeight(TreeNode* root) {
+        int h = 0;
+
+        while (root != NULL) {
+            h++;
+            root = root->left;
+        }
+
+        return h;
+    }
+
+    int rightHeight(TreeNode* root) {
+        int h = 0;
+
+        while (root != NULL) {
+            h++;
+            root = root->right;
+        }
+
+        return h;
+    }
+
+    int countNodes(TreeNode* root) {
+
+        if (root == NULL)
+            return 0;
+
+        int lh = leftHeight(root);
+        int rh = rightHeight(root);
+
+        if (lh == rh) {
+            return (1LL << lh) - 1;
+        }
+
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
